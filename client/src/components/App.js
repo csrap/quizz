2b9cddcf303442d04f528cee1613c7a67d2d40e9
César Pinto <caugusto3110@gmail.com>
2023-06-19 17:34:02 +0200

    update-v5

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,17 @@
 
 import '../styles/App.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+/** React routes */
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Root Element </div>
+  }
+])
+
 function App() {
   return (
     <div className="App">
